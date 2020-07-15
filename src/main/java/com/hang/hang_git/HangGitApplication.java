7a7601eaf3b72885840567e90c423d27947b0d0c
liuwh0715 <liuweihang0715@163.com>
2020-07-16 00:00:40 +0800

    first commit v.1.0

---
+++ src/main/java/com/hang/hang_git/HangGitApplication.java
@@ -0,0 +1,13 @@
+package com.hang.hang_git;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HangGitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HangGitApplication.class, args);
+    }
+
+}
